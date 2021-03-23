a574e95559bc8b8a4154c37247948adfcf850ca9
Adam Spann <Bas-Man@users.noreply.github.com>
2021-03-23 12:42:41 +0900

    basic

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+//use std::net::*;
+use trust_dns_resolver::config::*;
+use trust_dns_resolver::Resolver;
+
+fn main() {
+    // Construct a new Resolver with default configuration options
+    let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+
+    // Lookup the IP addresses associated with a name.
+    // The final dot forces this to be an FQDN, otherwise the search rules as specified
+    //  in `ResolverOpts` will take effect. FQDN's are generally cheaper queries.
+    //let response = resolver.lookup_ip("example.com.").unwrap();
+    //let response = resolver.mx_lookup("test.com.").unwrap();
+    let response = resolver.mx_lookup("gmail.com.").unwrap();
+
+    // There can be many addresses associated with the name,
+    //  this can return IPv4 and/or IPv6 addresses
+    //let address = response.iter().next().expect("no addresses returned!");
+    //println!("{}", address);
+
+    let addresses = response.iter();
+    for record in addresses {
+        println!("{} {}", record.preference(), record.exchange())
+    }
+    //if address.is_ipv4() {
+    //    println!("test");
+    //    assert_eq!(address, IpAddr::V4(Ipv4Addr::new(93, 184, 216, 34)));
+    //} else {
+    //    println!("test2");
+    //    assert_eq!(address, IpAddr::V6(Ipv6Addr::new(0x2606, 0x2800, 0x220, 0x1, 0x248, 0x1893, 0x25c8, 0x1946)));
+    //}
+}
