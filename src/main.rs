22a1e7eb8ea5a2a3d82f30dbcd41765f624c9236
Adam Spann <Bas-Man@users.noreply.github.com>
2021-03-23 13:41:55 +0900

    initial solution

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,31 @@ fn main() {
     // The final dot forces this to be an FQDN, otherwise the search rules as specified
     //  in `ResolverOpts` will take effect. FQDN's are generally cheaper queries.
     //let response = resolver.lookup_ip("example.com.").unwrap();
-    //let response = resolver.mx_lookup("test.com.").unwrap();
-    let response = resolver.mx_lookup("gmail.com.").unwrap();
+    let mx_response = resolver.mx_lookup("hotmail.com.");
 
     // There can be many addresses associated with the name,
     //  this can return IPv4 and/or IPv6 addresses
     //let address = response.iter().next().expect("no addresses returned!");
     //println!("{}", address);
 
-    let addresses = response.iter();
-    for record in addresses {
-        println!("{} {}", record.preference(), record.exchange())
+    match mx_response {
+        Err(_) => println!("No Records"),
+        Ok(mx_response) => {
+            let addresses = mx_response.iter();
+            for record in addresses {
+                println!("{} {}", record.preference(), record.exchange());
+                let host_name = record.exchange();
+                let lookup_response = resolver.lookup_ip(host_name.to_string().as_str()).unwrap();
+                let addr_list = lookup_response.iter();
+                for addr in addr_list {
+                    if addr.is_ipv4() {
+                        println!("\tip4: {}", addr)
+                    } else {
+                        println!("\tip6: {}", addr)
+                    }
+                }
+            }
+        }
     }
     //if address.is_ipv4() {
     //    println!("test");
