0b3c11aacb38733278b1de450d700aa0fd014dab
Adam Spann <Bas-Man@users.noreply.github.com>
2021-04-06 21:29:53 +0900

    making some progress

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,77 @@ use trust_dns_resolver::{config::*, lookup::MxLookup, lookup::SoaLookup, lookup:
 #[derive(Default, Debug, Clone)]
 struct Includes {
     modifier: char,
-    include_string: String,
+    txt: String,
 }
 
 impl Includes {
-    fn new(modifier: char, include_string: String) -> Self {
-        Self {
-            modifier,
-            include_string,
+    fn new(modifier: char, txt: String) -> Self {
+        Self { modifier, txt }
+    }
+    fn as_string(&self) -> String {
+        let mut txt = String::new();
+        if self.modifier != '+' {
+            txt.push(self.modifier);
+        }
+        txt.push_str("include:");
+        txt.push_str(self.txt.as_str());
+        txt
+    }
+    fn is_pass(&self) -> bool {
+        if self.modifier == '+' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_fail(&self) -> bool {
+        if self.modifier == '-' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_softfail(&self) -> bool {
+        if self.modifier == '~' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_neutral(&self) -> bool {
+        if self.modifier == '?' {
+            true
+        } else {
+            false
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+struct A {
+    modifier: char,
+    txt: String,
+}
+
+impl A {
+    fn new(modifier: char, txt: String) -> Self {
+        Self { modifier, txt }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Mx {
+    modifier: char,
+    txt: String,
+}
+
+impl Mx {
+    fn new(modifier: char, txt: String) -> Self {
+        Self { modifier, txt }
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Ip4 {
     modifier: char,
     ip: IpNetwork,
@@ -27,6 +85,49 @@ struct Ip4 {
 impl Ip4 {
     fn new(modifier: char, ip: IpNetwork) -> Self {
         Self { modifier, ip }
+    }
+    fn as_string(&self) -> String {
+        self.ip.to_string()
+    }
+    fn as_spf(&self) -> String {
+        let mut ip4_string = String::new();
+        if self.modifier != '+' {
+            ip4_string.push(self.modifier);
+        }
+        ip4_string.push_str("ip4:");
+        ip4_string.push_str(self.ip.to_string().as_str());
+        ip4_string
+    }
+    fn as_ip(&self) -> IpNetwork {
+        self.ip
+    }
+    fn is_pass(&self) -> bool {
+        if self.modifier == '+' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_fail(&self) -> bool {
+        if self.modifier == '-' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_softfail(&self) -> bool {
+        if self.modifier == '~' {
+            true
+        } else {
+            false
+        }
+    }
+    fn is_neutral(&self) -> bool {
+        if self.modifier == '?' {
+            true
+        } else {
+            false
+        }
     }
 }
 
@@ -39,7 +140,7 @@ impl Default for Ip4 {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Ip6 {
     modifier: char,
     ip: IpNetwork,
@@ -66,10 +167,10 @@ struct Spf1 {
     include: Option<Vec<Includes>>,
     redirect: Option<String>,
     is_redirected: bool,
+    a: Option<Vec<A>>,
+    mx: Option<Vec<Mx>>,
     ip4: Option<Vec<Ip4>>,
     ip6: Option<Vec<Ip6>>,
-    a: Option<Option<Vec<String>>>,
-    mx: Option<Option<Vec<String>>>,
     all_modifier: char,
 }
 
@@ -80,10 +181,10 @@ impl Spf1 {
             include: None,
             redirect: None,
             is_redirected: false,
-            ip4: None,
-            ip6: None,
             a: None,
             mx: None,
+            ip4: None,
+            ip6: None,
             all_modifier: '+',
         }
     }
@@ -92,8 +193,9 @@ impl Spf1 {
         let mut vec_of_includes: Vec<Includes> = Vec::new();
         let mut vec_of_ip4: Vec<Ip4> = Vec::new();
         let mut vec_of_ip6: Vec<Ip6> = Vec::new();
+        //let mut vec_of_a: Vec<A> = Vec::new();
+        //let mut vec_of_mx: Vec<A> = Vec::new();
         for record in records {
-            // Check first character. Is it a Modifier (+,-,~,)
             if record.contains("redirect") {
                 let items = record.rsplit("=");
                 for item in items {
@@ -101,33 +203,31 @@ impl Spf1 {
                     break;
                 }
                 self.is_redirected = true;
-            } else if record.contains("include") {
-                let modifier = return_modifier(record, 'i');
-                let items = record.rsplit(":");
-                for item in items {
-                    vec_of_includes.push(Includes::new(modifier, item.to_string()));
-                    break;
+            } else if record.contains("include:") {
+                let mod_and_modified_str = return_and_remove_modifier(record, 'i');
+                for item in record.rsplit(":") {
+                    vec_of_includes.push(Includes::new(mod_and_modified_str.0, item.to_string()));
+                    break; // skip the 'include:'
                 }
-            } else if record.contains("ip4") {
-                let modifier = return_modifier(record, 'i');
-                let ips = record.rsplit(":");
-                for ip in ips {
-                    let network: Ip4 = Ip4::new(modifier, ip.parse().unwrap());
+            } else if record.contains("ip4:") {
+                let mod_and_modified_str = return_and_remove_modifier(record, 'i');
+                if let Some(raw_ip4) = mod_and_modified_str.1.strip_prefix("ip4:") {
+                    let network: Ip4 = Ip4::new(mod_and_modified_str.0, raw_ip4.parse().unwrap());
                     vec_of_ip4.push(network);
-                    break;
                 }
-            } else if record.contains("ip6") {
-                // IP6 uses many ':' charcters. Normal split is difficult.
-                // Also still needs to handle the case where "ip6:" is not the complete prefix.
-                let modifier = return_modifier(record, 'i');
-                if let Some(raw_ip6) = record.strip_prefix("ip6:") {
-                    let network: Ip6 = Ip6::new(modifier, raw_ip6.parse().unwrap());
+            } else if record.contains("ip6:") {
+                let mod_and_modified_str = return_and_remove_modifier(record, 'i');
+                if let Some(raw_ip6) = mod_and_modified_str.1.strip_prefix("ip6:") {
+                    let network: Ip6 = Ip6::new(mod_and_modified_str.0, raw_ip6.parse().unwrap());
                     vec_of_ip6.push(network);
                 }
             } else if record.ends_with("all") {
-                self.all_modifier = return_modifier(record, 'a');
+                self.all_modifier = return_and_remove_modifier(record, 'a').0;
             }
         }
+        //if vec_of_a.len() > 0 {
+        //    self.a = Some(vec_of_a);
+        //}
         if vec_of_includes.len() > 0 {
             self.include = Some(vec_of_includes);
         };
@@ -139,25 +239,98 @@ impl Spf1 {
         };
     }
 
-    fn spf1(&self) -> String {
-        self.source.clone()
+    fn spf_source(&self) -> &String {
+        &self.source
+    }
+    fn clone_spf(&self) -> &Spf1 {
+        self.clone()
     }
 
-    fn list_of_includes(&self) -> Option<Vec<Includes>> {
+    fn includes(&self) -> Option<Vec<Includes>> {
         self.include.clone()
+    }
+    fn list_includes(&self) {
+        match &self.include {
+            None => println!("There are no include elements"),
+            Some(elements) => {
+                println!("Include Elements:");
+                for element in elements {
+                    println!("{}", element.as_string());
+                }
+            }
+        }
+    }
+    fn ip4(&self) -> Option<Vec<Ip4>> {
+        self.ip4.clone()
+    }
+    fn ip4_networks(&self) {
+        match &self.ip4 {
+            None => println!("There are no ip4 networks"),
+            Some(record) => {
+                println!("List of ip4 networks/hosts:");
+                for item in record {
+                    println!("{}", item.as_string());
+                }
+            }
+        }
+    }
+    fn ip4_spf_strings(&self) {
+        match &self.ip4 {
+            None => println!("There are no ip4 spf records."),
+            Some(records) => {
+                println!("List of spf ip4 records:");
+                for record in records {
+                    println!("{}", record.as_spf())
+                }
+            }
+        }
+    }
+    fn ip6(&self) -> Option<Vec<Ip6>> {
+        self.ip6.clone()
     }
 
     fn is_redirect(&self) -> bool {
         self.is_redirected
     }
+    fn redirect(&self) -> String {
+        if self.is_redirect() {
+            self.redirect.as_ref().unwrap().to_string()
+        } else {
+            String::from("")
+        }
+    }
+    fn redirect_as_spf(&self) -> Option<String> {
+        if self.is_redirect() {
+            let mut txt = String::new();
+            txt.push_str("redirect:");
+            txt.push_str(self.redirect.as_ref().unwrap().as_str());
+            Some(txt)
+        } else {
+            None
+        }
+    }
 }
 
-fn return_modifier(record: &str, c: char) -> char {
+// Check if the initial character in the string `record` matches `c`
+// If they do no match then return the initial character
+// if c matches first character of record, we can `+`, a blank modiifer equates to `+`
+fn return_and_remove_modifier(record: &str, c: char) -> (char, &str) {
+    // Returns a tuple of (modifier, &str)
+    // &str will have had the modifier character removed if it existed. The &str will be unchanged
+    // if the modifier was not present
     if c != record.chars().nth(0).unwrap() {
-        record.chars().nth(0).unwrap()
+        // Modifier exists. return tuple of modifier and `record` with modifier removed.
+        (record.chars().nth(0).unwrap(), remove_modifier(record))
     } else {
-        '+'
+        // Modifier does not exist, default to `+` and return unmodified `record`
+        ('+', record)
     }
+}
+fn remove_modifier(record: &str) -> &str {
+    // Remove leading (+,-,~,?) character
+    let mut chars = record.chars();
+    chars.next();
+    chars.as_str()
 }
 
 fn main() {
@@ -168,7 +341,7 @@ fn main() {
     // The final dot forces this to be an FQDN, otherwise the search rules as specified
     //  in `ResolverOpts` will take effect. FQDN's are generally cheaper queries.
     //let response = resolver.lookup_ip("example.com.").unwrap();
-    let query = "gmail.com.";
+    let query = "hotmail.com.";
     //let mx_response = resolver.mx_lookup(query);
     //let soa_response = resolver.soa_lookup(query);
     let txt_response = resolver.txt_lookup(query);
@@ -178,23 +351,33 @@ fn main() {
     let mut data = display_txt(&txt_response);
     data.parse();
     println!("{:?}", data);
-    println!("SPF1: {}", data.spf1());
-    println!("{:?}", data.list_of_includes());
+    println!("SPF1: {}", data.spf_source());
+    println!("{:?}", data.includes());
+    data.list_includes();
+    data.ip4_networks();
+    data.ip4_spf_strings();
     println!("Is a redirect: {}", data.is_redirect());
+    if data.is_redirect() {
+        println!("redirect: {}", data.redirect());
+        println!(
+            "{}",
+            data.redirect_as_spf()
+                .unwrap_or("Not a redirect.".to_string())
+        );
+    }
 }
 
 fn display_txt(txt_response: &ResolveResult<TxtLookup>) -> Spf1 {
-    let mut data = Spf1::new(&"".to_string());
+    let mut data = Spf1::default();
     match txt_response {
         Err(_) => println!("No TXT Records."),
         Ok(txt_response) => {
             let mut i = 1;
             for record in txt_response.iter() {
                 println!("TXT Record {}:", i);
-                let my_txt = &record.to_string();
-                println!("{}", my_txt);
-                if my_txt.starts_with("v=spf1") {
-                    data = Spf1::new(&my_txt.to_string());
+                println!("{}", &record.to_string());
+                if record.to_string().starts_with("v=spf1") {
+                    data = Spf1::new(&record.to_string());
                 }
                 println!("");
                 i = i + 1;
