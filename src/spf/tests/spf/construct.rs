8efd54571bd566c67a3b0e5e79a3a8df2162a8c3
Adam.S <Bas-Man@users.noreply.github.com>
2021-06-08 15:17:55 +0900

    update contstruct.rs make -> set

--- src/spf/tests/spf/construct.rs
+++ src/spf/tests/spf/construct.rs
@@ -6,7 +6,7 @@ mod construct {
     #[test]
     fn make_v1() {
         let mut spf = Spf::new();
-        spf.make_v1();
+        spf.set_v1();
         assert_eq!(spf.version, "v=spf1");
         assert_eq!(spf.version(), "v=spf1");
         assert_eq!(spf.is_v1(), true);
@@ -14,7 +14,7 @@ mod construct {
     #[test]
     fn make_v2_pra() {
         let mut spf = Spf::new();
-        spf.make_v2_pra();
+        spf.set_v2_pra();
         assert_eq!(spf.version, "spf2.0/pra");
         assert_eq!(spf.is_v2(), true);
         assert_eq!(spf.version(), "spf2.0/pra")
@@ -22,21 +22,21 @@ mod construct {
     #[test]
     fn make_v2_mfrom() {
         let mut spf = Spf::new();
-        spf.make_v2_mfrom();
+        spf.set_v2_mfrom();
         assert_eq!(spf.version, "spf2.0/mfrom");
         assert_eq!(spf.is_v2(), true);
     }
     #[test]
     fn make_v2_mfrom_pra() {
         let mut spf = Spf::new();
-        spf.make_v2_mfrom_pra();
+        spf.set_v2_mfrom_pra();
         assert_eq!(spf.version, "spf2.0/mfrom,pra");
         assert_eq!(spf.is_v2(), true);
     }
     #[test]
     fn make_v2_pra_mfrom() {
         let mut spf = Spf::new();
-        spf.make_v2_pra_mfrom();
+        spf.set_v2_pra_mfrom();
         assert_eq!(spf.version, "spf2.0/pra,mfrom");
         assert_eq!(spf.is_v2(), true);
     }
