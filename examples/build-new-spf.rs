1d4605ee7fd9caef279f126a2532827129e6d7f5
Adam.S <Bas-Man@users.noreply.github.com>
2021-10-12 15:07:19 +0900

    Rename example program

---
+++ examples/build-new-spf.rs
@@ -0,0 +1,38 @@
+use decon_spf::spf::kinds::MechanismKind;
+use decon_spf::spf::mechanism::Mechanism;
+use decon_spf::spf::qualifier::Qualifier;
+use decon_spf::spf::Spf;
+
+fn main() {
+    let mut spf1 = Spf::new();
+    spf1.set_v1();
+    spf1.append_ip_mechanism(Mechanism::new_ip(
+        Qualifier::Pass,
+        "203.32.160.0/32".parse().unwrap(),
+    ));
+
+    println!("New spf 1: >{}<", spf1.to_string());
+
+    let mut spf2 = Spf::new();
+    spf2.set_v1();
+    let ip = "203.32.166.0/24".parse().unwrap();
+    spf2.append_ip_mechanism(Mechanism::new_ip(Qualifier::Pass, ip));
+
+    println!("\nNew spf 2: >{}<", spf2.to_string());
+    println!("Add mx to spf2");
+    spf2.append_mechanism(Mechanism::new_mx_without_mechanism(Qualifier::Pass));
+    println!("Altered spf 2: >{}<", spf2.to_string());
+    println!("Clear mx from spf2");
+    spf2.clear_mechanism(MechanismKind::MX);
+    println!("Altered spf 2: >{}<", spf2.to_string());
+
+    let mut spf3 = Spf::new();
+    spf3.set_v2_pra();
+    spf3.append_mechanism(Mechanism::new_a_without_mechanism(Qualifier::Pass));
+    spf3.append_mechanism(Mechanism::new_all(Qualifier::Neutral));
+
+    println!("\nNew spf 3: >{}<", spf3.to_string());
+    println!("Change spf3 all to Fail");
+    spf3.append_mechanism(Mechanism::new_all(Qualifier::Fail));
+    println!("Altered spf 3: >{}<", spf3.to_string());
+}
