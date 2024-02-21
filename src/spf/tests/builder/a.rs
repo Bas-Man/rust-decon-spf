8492ca04f38bbcea3174c58b6cabd4f89f8dc435
Adam.S <Bas-Man@users.noreply.github.com>
2024-02-21 09:08:08 +0900

    test: builder::a basic cases

--- src/spf/tests/builder/a.rs
+++ src/spf/tests/builder/a.rs
@@ -1,73 +1,72 @@
 use crate::spf::SpfBuilder;
+use crate::{Mechanism, Qualifier};
 
-#[test]
-fn mechanism() {
-    let input = "v=spf1 a ~all";
-
-    let spf: SpfBuilder = input.parse().unwrap();
-    assert_eq!(spf.is_valid(), false);
-    assert_eq!(spf.version(), "v=spf1");
-    assert!(spf.a().is_some());
-    assert_eq!(spf.a().unwrap()[0].qualifier().is_pass(), true);
-    assert_eq!(spf.a().unwrap()[0].to_string(), "a");
-    assert_eq!(spf.all().unwrap().qualifier().is_softfail(), true);
-    assert_eq!(spf.all().unwrap().to_string(), "~all");
-}
-#[test]
-fn mechanism_slash_cidr() {
-    let input = "v=spf1 -a/24 ~all";
-
-    let spf: SpfBuilder = input.parse().unwrap();
-    assert!(spf.a().is_some());
-    assert_eq!(spf.a().unwrap()[0].qualifier().is_fail(), true);
-    assert_eq!(spf.a().unwrap()[0].to_string(), "-a/24");
-}
-#[test]
-fn mechanism_colon_domain() {
-    let input = "v=spf1 ?a:example.com ~all";
-
-    let spf: SpfBuilder = input.parse().unwrap();
-    assert!(spf.a().is_some());
-    assert_eq!(spf.a().unwrap()[0].qualifier().is_neutral(), true);
-    assert_eq!(spf.a().unwrap()[0].to_string(), "?a:example.com");
-}
-#[test]
-fn mechanism_domain_cidr() {
-    let input = "v=spf1 ~a:example.com/24 ~all";
-
-    let spf: SpfBuilder = input.parse().unwrap();
-    assert!(spf.a().is_some());
-    assert_eq!(spf.a().unwrap()[0].qualifier().is_softfail(), true);
-    assert_eq!(spf.a().unwrap()[0].to_string(), "~a:example.com/24");
-}
-mod invalid {
-    use crate::mechanism::MechanismError;
-    use crate::spf::{SpfBuilder, SpfError};
+mod valid {
+    use super::*;
+    #[test]
+    fn default() {
+        let mut spf: SpfBuilder = SpfBuilder::new();
+        spf.set_v1();
+        spf.append_mechanism_of_a(Mechanism::a(Qualifier::Pass));
+        spf.append_mechanism_of_all(Mechanism::all(Qualifier::SoftFail));
+        assert!(spf.a().is_some());
+        assert_eq!(spf.a().unwrap()[0].qualifier().is_pass(), true);
+        assert_eq!(spf.a().unwrap()[0].to_string(), "a");
+        assert_eq!(spf.all().unwrap().qualifier().is_softfail(), true);
+        assert_eq!(spf.all().unwrap().to_string(), "~all");
+        let new_spf = spf.build().unwrap();
+        assert_eq!(new_spf.version, "v=spf1");
+        assert_eq!(new_spf.mechanisms.len(), 2);
+        assert_eq!(new_spf.mechanisms[0].to_string(), "a");
+        assert_eq!(new_spf.mechanisms[1].to_string(), "~all");
+        assert_eq!(new_spf.all_idx, 1);
+    }
 
     #[test]
-    fn invalid_with_colon_only() {
-        let input = "v=spf1 a: -all";
-        let err_mechanism = "a:";
+    fn mechanism_slash_cidr() {
+        let mut spf: SpfBuilder = SpfBuilder::new();
+        spf.set_v1();
+        spf.append_mechanism(Mechanism::a(Qualifier::Fail).with_rrdata("/24").unwrap());
+        assert!(spf.a().is_some());
+        assert_eq!(spf.a().unwrap()[0].qualifier().is_fail(), true);
+        assert_eq!(spf.a().unwrap()[0].to_string(), "-a/24");
+        let new_spf = spf.build().unwrap();
+        assert_eq!(new_spf.mechanisms[0].to_string(), "-a/24");
+        assert_eq!(new_spf.mechanisms.len(), 1);
+    }
 
-        let err = input.parse::<SpfBuilder>().unwrap_err();
-        assert_eq!(
-            err,
-            SpfError::InvalidMechanism(MechanismError::InvalidMechanismFormat(
-                err_mechanism.to_string()
-            ))
-        )
+    #[test]
+    fn mechanism_colon_domain() {
+        let mut spf: SpfBuilder = SpfBuilder::new();
+        spf.append_mechanism_of_a(
+            Mechanism::a(Qualifier::Neutral)
+                .with_rrdata("example.com")
+                .unwrap(),
+        );
+        spf.append_mechanism_of_all(Mechanism::all(Qualifier::SoftFail));
+        assert!(spf.a().is_some());
+        assert_eq!(spf.a().unwrap()[0].qualifier().is_neutral(), true);
+        assert_eq!(spf.a().unwrap()[0].to_string(), "?a:example.com");
+        let new_spf = spf.build().unwrap();
+        assert_eq!(new_spf.mechanisms[0].to_string(), "?a:example.com");
+        assert_eq!(new_spf.mechanisms[1].to_string(), "~all");
+        assert_eq!(new_spf.mechanisms.len(), 2);
+        dbg!(&new_spf);
     }
     #[test]
-    fn invalid_with_slash_only() {
-        let input = "v=spf1 a/ -all";
-        let err_mechanism = "a/";
-
-        let err = input.parse::<SpfBuilder>().unwrap_err();
-        assert_eq!(
-            err,
-            SpfError::InvalidMechanism(MechanismError::InvalidMechanismFormat(
-                err_mechanism.to_string()
-            ))
-        )
+    fn mechanism_domain_cidr() {
+        let mut spf: SpfBuilder = SpfBuilder::new();
+        spf.set_v1().append_mechanism(
+            Mechanism::a(Qualifier::SoftFail)
+                .with_rrdata("example.com/24")
+                .unwrap(),
+        );
+        assert!(spf.a().is_some());
+        assert_eq!(spf.a().unwrap()[0].qualifier().is_softfail(), true);
+        assert_eq!(spf.a().unwrap()[0].to_string(), "~a:example.com/24");
+        let new_spf = spf.build().unwrap();
+        assert_eq!(new_spf.mechanisms[0].to_string(), "~a:example.com/24");
+        assert_eq!(new_spf.mechanisms.len(), 1);
+        dbg!(&new_spf);
     }
 }
