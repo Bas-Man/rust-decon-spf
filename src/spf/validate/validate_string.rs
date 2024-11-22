a44331b9d1513086ce003b84fe7921aada2ed069
Adam.S <Bas-Man@users.noreply.github.com>
2024-11-22 22:13:14 +0900

    chore(doc): Documentation

--- src/spf/validate/validate_string.rs
+++ src/spf/validate/validate_string.rs
@@ -3,6 +3,7 @@ use crate::spf::validate::Validate;
 use crate::{Spf, SpfError};
 
 impl Validate for Spf<String> {
+    /// Validate that the version is one that is of a known type.
     fn validate_version(&self) -> Result<(), SpfError> {
         if self.version.starts_with(crate::core::SPF1)
             || self.version.starts_with(crate::core::SPF2_PRA)
@@ -16,6 +17,8 @@ impl Validate for Spf<String> {
         }
     }
 
+    /// Check that Spf length does not exceed 512 bytes.
+    /// if this length is exceeded DNS packet may fail.
     fn validate_length(&self) -> Result<(), SpfError> {
         if self.source.len() > crate::core::MAX_SPF_STRING_LENGTH {
             return Err(SpfError::SourceLengthExceeded);
@@ -24,6 +27,11 @@ impl Validate for Spf<String> {
     }
 
     #[cfg(feature = "ptr")]
+    /// Check for the existence of a `ptr` mechanism.
+    /// # Results:
+    ///     Ok: If `ptr` is not present.
+    ///     SpfError::DeprecatedPtrPresent if present.
+    /// A `ptr` should generally not be used according to RFC...
     fn validate_ptr(&self) -> Result<(), SpfError> {
         for m in self.iter() {
             if m.kind() == &Kind::Ptr {
@@ -37,6 +45,7 @@ impl Validate for Spf<String> {
         todo!()
     }
 
+    /// Check that the number of looks up does not exceed the limit: 10
     fn validate_lookup_count(&self) -> Result<(), SpfError> {
         let mut count: u8 = 0;
         for m in self.iter() {
