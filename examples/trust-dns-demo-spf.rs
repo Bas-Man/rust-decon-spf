0ae626290424f604f5f619da32ec71cb47571143
Adam.S <Bas-Man@users.noreply.github.com>
2024-12-03 10:34:27 +0900

    chore(fix): Update code to show validate funciton in use

--- examples/trust-dns-demo-spf.rs
+++ examples/trust-dns-demo-spf.rs
@@ -27,8 +27,15 @@ fn main() {
     println!("Debug Output!");
     println!("{:?}", spf_record);
     println!("\nSource Attribute Output");
-    println!("SPF1: {}\n", spf_record.to_string());
-    spf_record.validate().expect("invalid spf");
+    print!("SPF1: {}", spf_record);
+    match spf_record.validate() {
+        Ok(_) => {
+            println!(" is Valid");
+        }
+        Err(_) => {
+            println!("is Invalid")
+        }
+    }
 }
 
 fn display_txt(query: &str, txt_response: &ResolveResult<TxtLookup>) -> Spf<String> {
