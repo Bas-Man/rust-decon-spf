7086cc5d69dfffe40e51b008a1465bb32d09c91e
Adam.S <Bas-Man@users.noreply.github.com>
2025-05-08 09:51:24 +0900

    fix: Bump Max SPF String length
    Domain me.com uses a very long v=spf1 string. There is no use of
    "include:" mechanism. Increased MAX_SPF_STRING_LENGTH from 512 to 1024

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -5,7 +5,7 @@ use regex::Regex;
 pub(crate) mod spf_regex;
 
 /// This is the maximum number of characters that an Spf Record can store.
-pub(crate) const MAX_SPF_STRING_LENGTH: usize = 512;
+pub(crate) const MAX_SPF_STRING_LENGTH: usize = 1024;
 pub(crate) const DNS_LOOKUP_LIMIT: usize = 10;
 
 /// Spf Versions
